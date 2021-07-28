79631874787906092351ab4c3c279243c0b25cd5
hafsa man <hhmansuri@gmail.com>
2021-07-28 10:37:47 +0530

    registration changes

--- database/migrations/2021_07_22_124949_added_postcomments.php
+++ database/migrations/2021_07_22_124949_added_postcomments.php
@@ -4,7 +4,7 @@ use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
 
-class AddedPostcomment extends Migration
+class AddedPostcomments extends Migration
 {
     /**
      * Run the migrations.
