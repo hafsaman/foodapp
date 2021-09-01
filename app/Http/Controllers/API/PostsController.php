7fed5c8c79ff604ebf83089ea3b16a08af6add34
Dilpreet Bajwa <dilpreet.seraphic@gmail.com>
2021-09-01 17:14:33 +0530

    post user notification migration correction

--- app/Http/Controllers/API/PostsController.php
+++ app/Http/Controllers/API/PostsController.php
@@ -412,6 +412,7 @@ class PostsController extends BaseController
   //validator place
      // return response()->json($request->postid, 200);
         $posts = Posts::where('id',$request->postid)->pluck('id');
+        $postsdata = Posts::where('id',$request->postid)->first();
      
         if(isset($posts)){
 
@@ -420,7 +421,7 @@ class PostsController extends BaseController
    		 	  $input['user_id'] = Auth::user()->id;
           $Post_comment=Posts_Comments::create($input);
           $post_comment1 =Posts_Comments::where('posts_comments.id',$Post_comment->id)->join('users','users.id','=','posts_comments.user_id')->select('posts_comments.id','posts_comments.post_id','posts_comments.user_id','posts_comments.comment','posts_comments.created_at','users.name','users.email','users.avatar')->first();
-            $inputnot['user_id']=$posts->user_id;
+            $inputnot['user_id']=$postsdata->user_id;
             $inputnot['description']= Auth::user()->name." Commented on Your Post";
             $inputnot['postlikeby_userid']=Auth::user()->id;
             $inputnot['post_id']=$request->postid;
