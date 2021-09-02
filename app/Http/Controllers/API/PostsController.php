9f1e9a50fcf3a944ed2bc6f503af4696464df38a
Dilpreet Bajwa <dilpreet.seraphic@gmail.com>
2021-09-02 17:28:47 +0530

    new changes

--- app/Http/Controllers/API/PostsController.php
+++ app/Http/Controllers/API/PostsController.php
@@ -568,7 +568,7 @@ class PostsController extends BaseController
     public function discover(Request $request){
      
       $posts_likes_data =Posts_likes::select('post_id', DB::raw('count(id) as total_likes'))
-             ->groupBy('total_likespost_id')->with('Posts')
+             ->groupBy('post_id')->with('Posts')
              ->orderBy('total_likes')
              ->paginate(10);
         if($posts_likes_data){
