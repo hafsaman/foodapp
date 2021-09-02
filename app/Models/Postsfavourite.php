d90e3bd66a46473268eb45e77691ac4a25ebc234
Dilpreet Bajwa <dilpreet.seraphic@gmail.com>
2021-09-02 18:00:18 +0530

    new changes add fav title

--- app/Models/Postsfavourite.php
+++ app/Models/Postsfavourite.php
@@ -4,12 +4,21 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use App\Models\Posts;
 
 class Postsfavourite extends Model
 {
     use HasFactory;
     protected $table='posts_favourite';
+    protected $appends = ['post_data'];
     protected $fillable = [
         'id',  'user_id','post_id','favourite'
     ];
+
+     public function getPostDataAttribute(){
+
+        return Posts::where('id',$this->post_id)->first();
+    }
+
+
 }
