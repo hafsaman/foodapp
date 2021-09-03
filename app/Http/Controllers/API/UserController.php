f0e9f83d39163f301a360a2c23f116daf2e2c518
Dilpreet Bajwa <dilpreet.seraphic@gmail.com>
2021-09-03 16:10:03 +0530

    add_changes

--- app/Http/Controllers/API/UserController.php
+++ app/Http/Controllers/API/UserController.php
@@ -289,7 +289,9 @@ class UserController extends BaseController
 
           $result = array();
 
-           $result['current_page'] = $user_follower->current_page;
+          return $user_follower;
+
+           $result['current_page'] = $user_follower;->current_page;
            $result['data'] =  $user_follower_data;
            $result['from'] = $user_follower->from;
            $result['last_page'] = $user_follower->last_page;
