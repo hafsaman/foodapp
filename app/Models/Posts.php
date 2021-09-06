2dd7224b52dbdedfa84d09cc0cbb5222d52bc9e4
Dilpreet Bajwa <dilpreet.seraphic@gmail.com>
2021-09-06 11:09:53 +0530

    commit gallary

--- app/Models/Posts.php
+++ app/Models/Posts.php
@@ -4,12 +4,19 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use App\Models\Posts_Gallary;
 
 class Posts extends Model
 {
     use HasFactory;
      protected $table='posts';
+     protected $appends = ['post_gallary'];
     protected $fillable = [
         'id',  'title',  'media_path','user_id','comment','is_shopping','price','region'
     ];
+
+     public function getPostGallaryAttribute(){
+
+        return Posts_Gallary::where('post_id',$this->post_id)->get();
+    }
 }
