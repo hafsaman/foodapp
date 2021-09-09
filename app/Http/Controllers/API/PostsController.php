e45ec03bcc09b4a9ed7692bb95d2ec1675796654
Dilpreet Bajwa <dilpreet.seraphic@gmail.com>
2021-09-09 15:53:02 +0530

    commit

--- app/Http/Controllers/API/PostsController.php
+++ app/Http/Controllers/API/PostsController.php
@@ -247,7 +247,7 @@ class PostsController extends BaseController
            $user_ids =  User::join('ratings','users.id','=','ratings.user_id')
           ->select('users.*',DB::raw('avg(ratings.rate) as rating'))
           ->groupBy('users.id')
-          ->having('rating, 4.0000')
+          ->havingRaw('avg(ratings.rate) = 4.0000')
           ->get();
 
 
