e1d142e6741434861d2d6ddff7caa882c41e26b4
Dilpreet Bajwa <dilpreet.seraphic@gmail.com>
2021-09-16 12:24:24 +0530

    commit

--- app/Models/Posts.php
+++ app/Models/Posts.php
@@ -5,6 +5,7 @@ namespace App\Models;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 use App\Models\Posts_Gallary;
+use App\Models\Region;
 use Auth;
 
 class Posts extends Model
