bc6099cc95e97e96fe5ddf87f02dc1c26c8090ef
Dilpreet Bajwa <dilpreet.seraphic@gmail.com>
2021-10-01 15:16:52 +0530

    commit

--- app/Models/Posts.php
+++ app/Models/Posts.php
@@ -6,13 +6,14 @@ use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 use App\Models\Posts_Gallary;
 use App\Models\Region;
+use App\Models\User;
 use Auth;
 
 class Posts extends Model
 {
     use HasFactory;
      protected $table='posts';
-     protected $appends = ['media_path','region_data'];
+     protected $appends = ['media_path','region_data','distance'];
     protected $fillable = [
         'id',  'title',  'media_path','user_id','comment','is_shopping','price','region','seasonal','unit'
     ];
@@ -26,9 +27,29 @@ class Posts extends Model
     	return $this->hasOne('App\Models\User', 'id', 'user_id');
     }
 
-     public function getRegionDataAttribute(){
+    public function getRegionDataAttribute(){
         return Region::where('region',$this->region)->first();
     }
 
+      public function getDistanceAttribute(){
+
+        $user_id = Auth::id();
+        $profile_user = User::where('id',$user_id)->first();
+        $post_user = User::where('id',$this->user_id)->first();
+        $profile_user_region = Region::where('region',$profile_user->region)->first();
+        $post_user_region = Region::where('region',$post_user->region)->first();
+        return Region::select(DB::raw($this->distance($profile_user_region->latitude, $profile_user_region->longitude,'distance'))) 
+                ->where('id',$post_user_region->id)
+                ->first(); 
+    }
+     function distance($lat,$long,$name)
+        {
+            //this distance is finded in KM
+            return "ROUND(6371 * acos(cos(radians(" . $lat . "))
+                    * cos(radians(latitude))
+                    * cos(radians(longitude) - radians(" . $long . "))
+                    + sin(radians(" .$lat. "))
+                    * sin(radians(latitude))),2) AS ".$name;
+        }
      
 }
